a51f6387ad218a3a3d72b4bb0ab16f92e464609b
Delfina Popelka <delfinapopelka@MacBook-Pro-de-Delfina-3.local>
2019-10-26 09:59:06 -0300

    mensaje

--- Workshop/javascriptp.js
+++ Workshop/javascriptp.js
@@ -22,15 +22,13 @@ function inputBlurHanlderDni (evento2) {
     var dniEvento = evento2.target
     var valorElementoDni = dniEvento.value
   
-    if ((valorElementoDni != '') && (valorElementoDni > 0 ) && busquedaDni(valorElementoDni, null) ){
+    if ((valorElementoDni != '') && (valorElementoDni > 0 ) /*&& busquedaDni(valorElementoDni, null)*/ ){
       dniEvento.classList.add('is-valid')
       dniEvento.classList.remove('is-invalid')
       /* DEJAR PARA DESPUES DNI
      valorElementoDni = localStorage.getItem(keyDni)
         */
-        
-
-  } else {
+    } else {
       dniEvento.classList.add('is-invalid')
       dniEvento.classList.remove('is-valid')
   }
@@ -53,3 +51,57 @@ return -1
 
 }
 
+function inputDeleteAlumno (dniIngresado, listaenbusqueda) {
+
+    for (var i = 0; i < listaenbusqueda.length; i++) {
+        var alumno = listaenbusqueda[posicion]
+        if (alumno.dni == dniIngresado ){
+            alumno.remove
+        } else {
+            return []
+        }
+    }
+}
+
+
+var dniAlumnoEliminar = document.getElementById('deleteDni')
+
+dniAlumnoEliminar.onclick = inputDeleteAlumno
+/*Como hacerlo para que sea en el click del boton*/
+
+function inputBlurHandlerEmail (evento3){
+    var emailEvento = evento3.target
+    var valorElementoEmail = emailEvento.value
+    var expresionRegular = /\w+@\w+\[a-z]/
+
+    if ((!expresionRegular.test(correo))){
+        valorElementoEmail.classList.add('is-valid')
+        valorElementoEmail.classList.remove('is-invalid')
+  } else {
+        valorElementoEmail.classList.add('is-invalid')
+        valorElementoEmail.classList.remove('is-valid')
+  }
+
+    
+
+
+}
+
+
+var email = document.getElementById('email')
+
+email.onblur = inputBlurHandlerEmail
+
+
+function agregarAlumnoOnClick (alumnoParaAgregar, listaenbusqueda) {
+    for (var i = 0; i < listaenbusqueda.length; i++) {
+        var alumnoParaAgregar = listaenbusqueda[posicion] {
+            if (alumnoParaAgregar != i) {
+                var crearNodo = document.getElementsByClassName
+                crearNodo.innerHTML = alumnoParaAgregar
+                } else {
+                /*No agrega nada*/
+                }
+                }
+
+}
